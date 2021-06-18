735e98c8904adcb2c8dddede5aef16d0e3e32bc7
KAMEGAWA Kazushi <kkamegawa@clueup.org>
2021-06-18 13:54:51 +0900

    add solutions

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# memoryeaterextension
+# Summary
+
 This repository sample extension for Visual Studio 2022 Preview.
+
+## System Requirement
+
+- Visual Studio 2022 Preview 1 above
+  
+## execute
+
+![memory 4GB over](images/memory_usage.png)
+
---
+++ memeater/AllocMemory.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Task = System.Threading.Tasks.Task;
+
+namespace memeater
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class AllocMemory
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("7b99917b-f7fb-4afb-9a38-dd7406df6d8d");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        private List<byte[]> DummyMemory;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AllocMemory"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private AllocMemory(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+            DummyMemory = new List<byte[]>();
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static AllocMemory Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in AllocMemory's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new AllocMemory(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            DummyMemory.Add(new byte[512 * 1024 * 1024]);
+        }
+    }
+}
